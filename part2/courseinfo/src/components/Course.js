91aee7bc0cc7124ce838a1073fb3744a6a61db3f
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-11-07 17:22:19 +0100

    2.1 to 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,9 +1,42 @@
-import React from 'react'
+const Course = (props) => {
+  const course = props.course;
 
-const Course = () => {
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
+  };
+
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    );
+  };
+
+  const Content = (props) => {
+    return (
+      <div>
+        {props.parts.map((part) => {
+          return <Part key={part.id} part={part} />;
+        })}
+      </div>
+    );
+  };
+
+  const Total = (props) => {
+    const parts = props.parts.map((course) => course.exercises);
+    const sum = parts.reduce((s, p) => s + p);
+
+    return <p style={{ fontWeight: 'bold' }}>Number of exercises {sum}</p>;
+  };
+
   return (
-    <div>Course</div>
-  )
-}
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
 
-export default Course
+export default Course;
