e8ae6916ee367b10cc7eccbbe1d0180ef743324a
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-11-10 09:28:39 +0100

    2.19 - 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getAll, createPerson, deletePerson, updatePerson } from './services/Persons';
+import Notification from './components/Notification';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
@@ -10,6 +11,10 @@ const App = () => {
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  const [notification, setNotification] = useState({
+    message: null,
+    error: null,
+  });
 
   // LOADS INITIAL SET OF DATA
   useEffect(() => {
@@ -36,6 +41,7 @@ const App = () => {
         )
       ) {
         updateNumber(filteredPerson[0].id, nameObject);
+        setNotification({ message: 'Phone Number has been updated.', error: 'success' });
         setNewName('');
         setNewNumber('');
         return;
@@ -45,6 +51,7 @@ const App = () => {
 
     createPerson(nameObject).then((response) => {
       setPersons(persons.concat(response.data));
+      setNotification({ message: `Added ${newName}`, error: 'success' });
       setNewName('');
       setNewNumber('');
     });
@@ -55,9 +62,20 @@ const App = () => {
     const filter = persons.filter((person) => person.id === +deleteId)[0];
     const index = persons.indexOf(filter);
     if (window.confirm(`Do you really want to delete ${filter.name}?`)) {
-      deletePerson(deleteId).then(
-        setPersons([...persons.slice(0, index), ...persons.slice(index + 1)])
-      );
+      deletePerson(deleteId)
+        .then(setPersons([...persons.slice(0, index), ...persons.slice(index + 1)]))
+        .then(
+          setNotification({
+            message: `${filter.name} was successfully removed from the server`,
+            error: 'success',
+          })
+        )
+        .catch((error) => {
+          setNotification({
+            message: `The information of ${filter.name} was already removed from the server`,
+            error: 'error',
+          });
+        });
     }
   };
 
@@ -84,6 +102,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification} />
       <Filter value={filter} onChange={handleFilter} />
       <h2>add a new</h2>
       <PersonForm
