04c9b00311d824c3614b091fa7c7d52beac5e65a
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-11-12 15:46:56 +0100

    part4 initial commit - WIP

---
+++ part4/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['warn', 'always'],
+  },
+};
